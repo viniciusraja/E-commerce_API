0d1cdd77631ad60a80bffab117f6ee785af77fd1
Vinícius Rajá <viniciusrfl@hotmail.com>
2020-06-16 02:21:23 -0300

    ADD: Knex Migration Schema Product Image

---
+++ src/migrations/20200615010206_create_table_productImage.js
@@ -0,0 +1,18 @@
+  exports.up = function(knex) {
+    return knex.schema.createTable('product_image', table=>{
+        table.increments('id').primary()
+        table.integer('product_id').references('id')
+              .inTable('products').notNull().unique()
+        table.string('name').notNull()
+        table.decimal('size').notNull()
+        table.string('key')
+        table.string('url',1000).notNull()
+        table.datetime('created_at',{useTz:false}, {precision: 0}).defaultTo(knex.fn.now(0))
+    })
+  };
+  
+  exports.down = function(knex) {
+      return knex.schema.dropTable('product_image')
+    
+  };
+  
